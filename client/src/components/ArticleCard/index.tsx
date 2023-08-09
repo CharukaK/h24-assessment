d5df9fefebd93bfa7e79422a7186175d41d946f8
CharukaK <charukakarunanayake@gmail.com>
2023-08-09 13:47:31 +0530

    Add alt text to images shown

--- client/src/components/ArticleCard/index.tsx
+++ client/src/components/ArticleCard/index.tsx
@@ -5,13 +5,26 @@ interface ArticleCardProps {
     article: Article;
 }
 
+
+// export var ArticleCard = ({ article }: { article: Article }) => {
+//     return (
+//         <div className={'article'}>
+//             <img src={article.images[0].path} />
+//             <div>{article.name}</div>
+//             <div>{formatter.format(article.prices.regular.value / 100)}</div>
+//             <section role="button">Add to cart</section>
+//         </div>
+//     )
+// };
+//
+
 export function ArticleCard(props: ArticleCardProps) {
     const { article } = props;
 
     // TODO: fix intl for this component
     return (
         <div className={'article'}>
-            <img src={article.images[0].path} />
+            <img src={article.images[0].path} alt={article.name} />
             <div>{article.name}</div>
             <div>{article.prices.regular.value / 100}</div>
             <section role="button">Add to cart</section>
