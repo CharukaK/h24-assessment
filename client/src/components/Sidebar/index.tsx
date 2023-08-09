e926e269fe5a04dd04109292f9cfbb5ee04d699f
CharukaK <charukakarunanayake@gmail.com>
2023-08-09 21:49:25 +0530

    Improve side bar styling

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -1,3 +1,4 @@
+import styled from "@emotion/styled";
 import React from "react";
 import { Category } from "../../types";
 
@@ -5,19 +6,38 @@ interface SidebarProps {
     categories: Category[];
 }
 
+const SideBarContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: #fff;
+    max-height: 100vh;
+    overflow-y: auto;
+`;
+
+const CategoryItem = styled.li`
+    a {
+        text-decoration: none;
+        color: #5b5e61
+    }
+
+    a:hover {
+        color: #d14327;
+    }
+`;
+
 export function Sidebar(props: SidebarProps) {
     const { categories } = props;
     return (
-        <div className={'sidebar'}>
+        <SideBarContainer className={'sidebar'}>
             <h3>Kategorien</h3>
             {
                 categories.length ? (
                     <ul>
                         {categories[0].childrenCategories.list.map(({ name, urlPath }, i) => {
                             return (
-                                <li key={`${name.replaceAll(' ', '_').trim()}_${i}`}>
+                                <CategoryItem key={`${name.replaceAll(' ', '_').trim()}_${i}`}>
                                     <a href={`/${urlPath}`}>{name}</a>
-                                </li>
+                                </CategoryItem>
                             );
                         })}
                     </ul>
@@ -25,7 +45,7 @@ export function Sidebar(props: SidebarProps) {
                     'Loading...'
                 )
             }
-        </div>
+        </SideBarContainer>
     )
 }
 
