bded1b9b80cff7d3ac375b9095c99ff0232830f0
CharukaK <charukakarunanayake@gmail.com>
2023-08-10 18:13:51 +0530

    Fix Footer styling

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const FooterContainer = styled.div`
+    grid-area: footer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #00bfff;
+    padding: 0 20px;
+    height: 50px;
+    color: #fff;
+`;
 
 export function Footer() {
     return (
-        <div className={'footer'}>
+        <FooterContainer>
             Alle Preise sind in Euro (€) inkl. gesetzlicher Umsatzsteuer und Versandkosten.
-        </div>
+        </FooterContainer>
     )
 }
 
